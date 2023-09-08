cdbffeffe7557f2f67cd665d6750e5eaf9b1e53b
Laurits Kure <thekure@Lauritss-iMac.local>
2023-09-08 17:56:13 +0200

    2.4, 2.5, 3.2 done

--- PSData/Assignment02/Assignment02/Intcomp1.fs
+++ PSData/Assignment02/Assignment02/Intcomp1.fs
@@ -378,3 +378,54 @@ let intsToFile (inss : int list) (fname : string) =
     System.IO.File.WriteAllText(fname, text);;
 
 (* -----------------------------------------------------------------  *)
+
+let sinstrToInt instr =
+    match instr with
+    | SCstI i -> [0; i]
+    | SVar i -> [1; i]
+    | SAdd -> [2]
+    | SSub -> [3]
+    | SMul -> [4]
+    | SPop -> [5]
+    | SSwap -> [6]
+    | _ -> failwith "unknown instruction"
+    
+let assemble instrList =
+    List.fold (fun acc instr -> acc @ (sinstrToInt instr)) [] instrList
+
+
+// Exercise 3.2: ^(b*ab+)*b*a?$
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
