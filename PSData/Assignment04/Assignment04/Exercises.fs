dd362ac3d5700067c11829becbc741c2134af0f9
Laurits <laku@itu.dk>
2023-09-22 14:32:31 +0200

    update letfun and call

--- PSData/Assignment04/Assignment04/Exercises.fs
+++ PSData/Assignment04/Assignment04/Exercises.fs
@@ -1,2 +1,18 @@
-module Assignment04.Exercises
+module Exercises
 
+let sum n =
+    List.fold (fun acc ele -> ele + acc) 0 [0..n]
+    
+let pow value exponent =
+    let rec aux e acc =
+        match e with
+        | 0 -> acc
+        | e -> aux (e-1) (acc*value)
+    aux exponent 1
+
+let pow2 value exponent =
+    let rec aux e acc =
+        match e with
+        | 0 -> acc
+        | e -> aux (e-1) (acc)
+    aux exponent 1
